Fix bug in gradient computation.
Also:
- document the gradient computation in more detail
- show progress bar
- tweak learning rate

sgd.py
@@ -1,4 +1,6 @@
 import argparse
+import sys
+import time
 
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
@@ -6,6 +8,7 @@ import numpy as np
 
 
 class Activation(object):
+    """A function that knows its own derivative."""
     def __call__(self, x):
         raise NotImplementedError()
 
@@ -49,18 +52,33 @@ class Layer(object):
         return self.output
 
     def backprop(self, partial_output):
-        # Given a partial derivative of some function wrt our output,
-        # return the partial derivative wrt our inputs.
-        # Also compute the derivative wrt our weights and biases, and
-        # save those for later.
+        """Given a partial derivative of some function wrt our output,
+        return the partial derivative wrt our inputs.
+
+        Also compute the derivative wrt our weights and biases, and
+        save those for later.
+
+        Args:
+            partial_output: partial derivative of the loss with respect
+                to our activation (dL / dh)
+        """
+        # This vector shows up in all 3 partial computations.
         error = partial_output * self.activation.derivative(self.affine_output)
-        partial_input = np.dot(self.w.T, error)
-        # dL / dw
+
+        # dL / dw = (dL / dh) * (dh / dw)
+        # h = sigma(wx + b)
+        # dh / dw = x * sigma'(wx + b)
         self.partial_w = np.dot(
-            partial_output.reshape((self.output_dim, 1)),
+            error.reshape((self.output_dim, 1)),
             self.x.reshape((1, self.input_dim)))
-        # dL / db
-        self.partial_b = partial_output
+
+        # dL / db = (dL / dh) * (dh / db)
+        # dh / db = sigma'(wx + b)
+        self.partial_b = error
+
+        # dL / dx = (dL / dh) * (dh / dx)
+        # dh / dx = w * sigma'(wx + b)
+        partial_input = np.dot(self.w.T, error)
         return partial_input
 
     def descend(self, learning_rate):
@@ -90,12 +108,12 @@ def save_graph(layers, output_file):
     X = np.array(X)
     Y = np.array(Y)
     Z = np.array(Z)
-    print X.shape, Y.shape, Z.shape
     Z -= np.min(Z)
     Z /= np.max(Z)
 
     cs = plt.contourf(X, Y, Z, np.arange(0, 1.1, 0.01), cmap=cm.viridis)
     plt.savefig(output_file)
+
 
 def main():
     X = np.load('x.npy')
@@ -106,7 +124,7 @@ def main():
     layer3 = Layer(12, 1, Linear())
 
     num_epochs = 100
-    learning_rate = 0.001
+    learning_rate = 0.002
     num_examples = X.shape[0]
 
     save_graph([layer1, layer2, layer3], 'my_sgd_plots/epoch_000.png')
@@ -114,7 +132,11 @@ def main():
     for i in range(num_epochs):
         print 'Epoch %d...' % (i + 1,)
         sum_squares = 0.0
+        start = time.time()
         for j in range(num_examples):
+            if (j + 1) % 5000 == 0:
+                sys.stdout.write('*')
+                sys.stdout.flush()
             x = X[j]
             expected = y[j]
             h1 = layer1.feed_forward(x)
@@ -132,9 +154,10 @@ def main():
             layer3.descend(learning_rate)
             layer2.descend(learning_rate)
             layer1.descend(learning_rate)
-        learning_rate *= 0.99
+        elapsed = time.time() - start
+        learning_rate *= 0.995
         mse = sum_squares / num_examples
-        print 'Complete. MSE %.06f' % (mse,)
+        print ' complete (%.1f seconds). MSE %.06f' % (elapsed, mse,)
         save_graph([layer1, layer2, layer3], 'my_sgd_plots/epoch_%03d.png' % (i + 1,))
 
 